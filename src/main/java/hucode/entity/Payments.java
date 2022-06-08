ea13cca9ef9680985f784691e151b6d7e2b481d5
hucodeuz96 <hucodeuz96@gmail.com>
2022-06-08 13:41:13 +0500

    small change

--- src/main/java/hucode/entity/Payments.java
+++ src/main/java/hucode/entity/Payments.java
@@ -3,5 +3,7 @@ package hucode.entity;
 public class Payments {
     public static void main(String[] args) {
         System.out.println("asddas");
+        System.out.println("asddas");
+        System.out.println("asddas");
     }
 }
